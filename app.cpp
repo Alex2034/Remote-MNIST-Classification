7248432fb685fb07a07946112da35417ec6c5da1
fedor1113 <fedor185@hotmail.com>
2023-10-29 23:34:52 +0300

    Use modern Torch and OpenCV syntax to match our lib versions

--- app.cpp
+++ app.cpp
@@ -8,33 +8,33 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-using namespace cv;
-
 // Function to display an image using OpenCV
-void displayImage(Mat img)
+void displayImage(cv::Mat img)
 {
-    namedWindow("Display window", CV_WINDOW_AUTOSIZE); // Create a window for display.
-    imshow("Display window", img);
-    waitKey(0);
+    cv::namedWindow("Display window"); // Create a window for display.
+    cv::imshow("Display window", img);
+    cv::waitKey(0);
 }
 
 
 int main(int argc, const char* argv[]) {
     //std::cout << "OpenCV version : " << CV_VERSION << std::endl;
-    Mat inputImage;
-    inputImage = imread("../MNISTsamples/img_63.jpg", CV_LOAD_IMAGE_GRAYSCALE); // Load the image in grayscale.
+    cv::Mat inputImage;
+    inputImage = cv::imread("../MNISTsamples/img_63.jpg",
+		cv::ImreadModes::IMREAD_GRAYSCALE); // Load the image in grayscale.
     displayImage(inputImage);
 
     // Deserialize the ScriptModule from a file using torch::jit::load().
-    std::shared_ptr<torch::jit::script::Module> torchModule = torch::jit::load("../model_trace.pt");
+    torch::jit::script::Module torchModule = torch::jit::load("../model_trace.pt");
 
     std::vector<int64_t> sizes = {1, 1, inputImage.rows, inputImage.cols};
     at::TensorOptions options(at::ScalarType::Byte);
-    at::Tensor tensorImage = torch::from_blob(inputImage.data, at::IntList(sizes), options);
+    at::Tensor tensorImage = torch::from_blob(inputImage.data,
+		at::IntList(sizes), options);
     tensorImage = tensorImage.toType(at::kFloat);
-    at::Tensor result = torchModule->forward({tensorImage}).toTensor();
+    at::Tensor result = torchModule.forward({tensorImage}).toTensor();
 
     auto maxResult = result.max(1, true);
     auto maxIndex = std::get<1>(maxResult).item<float>();
-    std::cout << maxIndex << std::endl;
+    std::cout << maxIndex << "\n";
 }
